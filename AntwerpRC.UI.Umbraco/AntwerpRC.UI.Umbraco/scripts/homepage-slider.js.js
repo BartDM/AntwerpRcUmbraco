ef6a56642761cb4e529e77fc65d0b255cf44b6e1
BartDM <bart@bartdemeyer.be>
2013-08-16 16:09:10 +0200

    Homepage en pagebase aanpassingen

---
+++ AntwerpRC.UI.Umbraco/AntwerpRC.UI.Umbraco/scripts/homepage-slider.js.js
@@ -0,0 +1,31 @@
+﻿$(document).ready(function() {
+    var sliders = $(".image-slider > .slider");
+
+    //shiftSlider(sliders, 0);    
+    $(sliders[0]).hide({ duration: 500 }, function () {
+        console.log("ready with transition");
+    });
+
+
+});
+
+function shiftSlider(container, nr) {
+    if (nr > container.length - 1) {
+        nr = 0;
+    }
+    var slider = container[nr];
+    $(slider).animate({ color: "red" }, { duration: 500 }, function () {
+        console.log("ready with transition");
+    });
+}
+
+/*
+            nr++;
+            if (nr > container.length - 1) {
+                nr = 0;
+            }
+            var sliderToShow = container[nr];
+            $(sliderToShow).animate({ display: "block" }, { duration: 5000 }, function () {
+                shiftSlider(nr++);
+            });
+*/
